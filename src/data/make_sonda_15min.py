829f1950e6eb50d7204b65b36553b34f12c0ccc6
Carlos Severiano <cseveriano@minds-imac-1.local>
2018-01-19 16:45:50 -0200

    Ajustes no gerador de base SONDA

--- src/data/make_sonda_15min.py
+++ src/data/make_sonda_15min.py
@@ -12,7 +12,13 @@ output_filepath = '../../data/processed/SONDA/'+pattern+'-15min.csv'
 
 header_file = '../../data/raw/SONDA/ED_header_new.csv'
 hd = read_csv(header_file, sep = ';')
-hd.to_csv(output_filepath, sep = ';')
+
+# mount new header
+cl = hd.columns[4:8].tolist()
+cl.extend(hd.columns[10:])
+cl.insert(0,"date")
+new_header = pd.DataFrame(columns = cl)
+new_header.to_csv(output_filepath, sep = ';', index=False)
 
 for file in glob.glob(input_filepath + '/' + pattern + '*'):
     print (file + '\n')
@@ -28,9 +34,9 @@ for file in glob.glob(input_filepath + '/' + pattern + '*'):
 
     date_col = pd.to_datetime(year * 1000 + day_of_year, format='%Y%j') + pd.TimedeltaIndex(minute_of_day, unit='m')
 
-    data = pd.DataFrame(df.glo_avg)
+    data = pd.concat([df.iloc[:, df.columns.get_loc('glo_avg'):df.columns.get_loc('par_avg')], df.iloc[:, df.columns.get_loc('tp_sfc'):]], axis=1)
     data.index = date_col
     data = data.resample('15min').mean()
     ## salvar arquivo output
-    data.to_csv(output_filepath, mode='a', header=False, sep = ';')
+    data.to_csv(output_filepath, mode='a', header=False, sep = ';', na_rep='NaN')
 
