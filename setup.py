3409871cdb7ef3d2a8e5a13933bf64cdeb4c4bbd
cseveriano <carlossjr@gmail.com>
2019-02-19 15:09:43 -0300

    Setup fix

--- setup.py
+++ setup.py
@@ -2,13 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='spatio-temporal-forecasting',
-       package_dir={'':'src'},
-      packages=find_packages("src"), # <- test is excluded
-
-    #package_dir={}
-    #package_data={'pyFTS.data': ['*.csv','*.csv.bz2']},
-    #data_files=[('data', ['pyFTS/data/Enrollments.csv', 'pyFTS/data/AirPassengers.csv'])],
-    #include_package_data=True,
+    package_dir={'':'src/models'},
+    packages=find_packages("src/models"),
     version='0.1',
     description='Spatio Temporal Forecasting',
     author='Carlos Severiano',
